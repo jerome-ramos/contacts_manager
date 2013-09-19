e3faabb5ef9052d04c91764995249d653cfdaa03
jerome-ramos <ramos.jeromeh@gmail.com>
2013-09-19 23:59:19 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,8 @@
 	var App = Backbone.View.extend({
 		el: "#contacts",
 		events: {
-			'click #add_contact': 'addPerson'
+			'click #add_contact': 'addPerson',
+		
 		},
 		initialize: function () {
 			this.input_name = $('#inputs input[name=fullname]');
@@ -27,9 +28,20 @@
 			var view = new PersonView({model: person});
 			this.contacts_list.append(view.render().el);
 		}
+	
+		
 	});
 
 	var PersonModel = Backbone.Model.extend({
+		validate: function(attrs) {
+		if ( ! attrs.input_name || ! attrs.input_username ) {
+			return 'A name and username name are required.';
+		}
+
+		if( isNaN(attrs.input_number) == true ){
+			return 'Please enter a valid number';
+		}
+		},
 		defaults: {
 			'name': '-',
 			'number': '-',
@@ -51,14 +63,37 @@
 	var PersonView = Backbone.View.extend({
 		tagName: 'tr',
 		template: $('#contact_template').html(),
+		events: {
+			
+			'click a.edit' : 	'editPerson',
+			'click a.delete' : 'deletePerson'
+		},
 		initialize: function() {
-
+				this.model.on('destroy', this.unrender, this);x`
 		},
 		render: function() {
 			var compiledTemplate = _.template(this.template);
 			this.$el.html(compiledTemplate(this.model.toJSON()))
 			return this;
+		},
+		deletePerson: function() {
+		this.collection.destroy();
+		},
+			
+		editPerson: function (evt) {
+
+			var person = new PersonModel({
+				name: this.input_name.val(),
+				number: this.input_number.val(),
+				username: this.input_username.val()
+				});
+			
+			this.collection.save(person);
+			
+			var view = new PersonView({model: person});
+			this.contacts_list.append(view.render().el);
 		}
+		
 	});
 
 	var contactApp = new App({collection: new PersonCollection()});
